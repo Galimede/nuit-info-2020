d93c7e1f936ad44ef3b748f4204b52ae95004c68
Francois Lannoy <flannoy154@gmail.com>
2020-12-03 21:02:08 +0100

    :rotating_light: disbled eslint for test file

--- api/test/test.js
+++ api/test/test.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import chai from 'chai';
-import request from 'request';
+// import request from 'request';
 
 const should = chai.should;
 
 describe('/route/', () => {
-   describe('/', () => {
-    it('should send stuff', () => {
+    describe('/', () => {
+        it('should send stuff', () => {
 
     });
   });
