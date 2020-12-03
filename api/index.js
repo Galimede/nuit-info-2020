5d0d5612bcd68c1bf902c64ff806a4e843effd7f
Francois Lannoy <flannoy154@gmail.com>
2020-12-03 19:21:16 +0100

    :heavy_plus_sign: added babel

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
-const express = require('express')
+import exampleRouter from './routes/route.js'
+import express from 'express'
 
 const app = express()
-
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+
+app.use('/example', exampleRouter)
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
---
+++ api/routes/route.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const exampleRouter = express.Router()
+
+exampleRouter.get('/', (req, res) => {
+    res.send('Birds home page')
+})
+
+export default exampleRouter
